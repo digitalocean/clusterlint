f63933a0ebbed6785f5c9c720164086151d9fdb8
Adam Wolfe Gordon <awg@digitalocean.com>
2019-06-11 14:14:04 -0400

    Create check registry with initial noop check, and vendor dependencies
    Factor the KubeObjects type out of package main and into the top-level
    clusterlint package. For now still populate it in main, though we'll
    likely want to pull this out later.
    
    Add a check type that checks will implement, and implement an initial
    noop check that satisfies it. Create a registry for checks.
    
    Vendor dependencies so that we can do a consistent build. For now we're
    depending on the master version of client-go since they don't have a
    release that supports modules yet, but we should move to a release once
    they do.

---
+++ checks/noop/noop.go
@@ -0,0 +1,35 @@
+package noop
+
+import (
+	"github.com/digitalocean/clusterlint"
+	"github.com/digitalocean/clusterlint/checks"
+)
+
+func init() {
+	checks.Register(&check{})
+}
+
+type check struct{}
+
+// Name returns a unique name for this check.
+func (nc *check) Name() string {
+	return "noop"
+}
+
+// Groups returns a list of group names this check should be part of.
+func (nc *check) Groups() []string {
+	return nil
+}
+
+// Description returns a detailed human-readable description of what this check
+// does.
+func (nc *check) Description() string {
+	return "Does not check anything. Returns no errors."
+}
+
+// Run runs this check on a set of Kubernetes objects. It can return warnings
+// (low-priority problems) and errors (high-priority problems) as well as an
+// error value indicating that the check failed to run.
+func (nc *check) Run(*clusterlint.KubeObjects) (warnings []error, errors []error, err error) {
+	return nil, nil, nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/digitalocean/clusterlint
+
+go 1.12
+
+require (
+	github.com/stretchr/testify v1.2.2
+	k8s.io/api v0.0.0-20190602125759-c1e9adbde704
+	k8s.io/apimachinery v0.0.0-20190602125621-c0632ccbde11
+	k8s.io/client-go v0.0.0-20190602130007-e65ca70987a6
+)
