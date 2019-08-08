d5dea303c552bceba97bb30d167e0a3ece0caaaa
ibrasho <me@ibrasho.com>
2019-08-09 00:36:33 +0300

    Load k8s client-go auth plugins
    Signed-off-by: ibrasho <me@ibrasho.com>

--- kube/objects.go
+++ kube/objects.go
@@ -26,6 +26,9 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+
+	// Load client-go authentication plugins
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 //Identifier is used to identify a specific namspace scoped object.
