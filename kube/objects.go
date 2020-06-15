6d9242c943b9db4335cf7b3adf441e1d6f93a63f
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2020-06-15 09:56:12 -0700

    Update api interactions to pass along context

--- kube/objects.go
+++ kube/objects.go
@@ -70,70 +70,69 @@ func (c *Client) FetchObjects(ctx context.Context, filter ObjectFilter) (*Object
 	opts := metav1.ListOptions{}
 	objects := &Objects{}
 
-	var g errgroup.Group
-
+	g, gCtx := errgroup.WithContext(ctx)
 	g.Go(func() (err error) {
-		objects.Nodes, err = client.Nodes().List(opts)
+		objects.Nodes, err = client.Nodes().List(gCtx, opts)
 		return
 	})
 	g.Go(func() (err error) {
-		objects.PersistentVolumes, err = client.PersistentVolumes().List(opts)
+		objects.PersistentVolumes, err = client.PersistentVolumes().List(gCtx, opts)
 		return
 	})
 	g.Go(func() (err error) {
-		objects.ComponentStatuses, err = client.ComponentStatuses().List(opts)
+		objects.ComponentStatuses, err = client.ComponentStatuses().List(gCtx, opts)
 		return
 	})
 	g.Go(func() (err error) {
-		objects.Pods, err = client.Pods(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.Pods, err = client.Pods(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.PodTemplates, err = client.PodTemplates(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.PodTemplates, err = client.PodTemplates(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.PersistentVolumeClaims, err = client.PersistentVolumeClaims(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.PersistentVolumeClaims, err = client.PersistentVolumeClaims(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.ConfigMaps, err = client.ConfigMaps(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.ConfigMaps, err = client.ConfigMaps(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.Secrets, err = client.Secrets(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.Secrets, err = client.Secrets(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.Services, err = client.Services(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.Services, err = client.Services(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.ServiceAccounts, err = client.ServiceAccounts(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.ServiceAccounts, err = client.ServiceAccounts(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.ResourceQuotas, err = client.ResourceQuotas(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.ResourceQuotas, err = client.ResourceQuotas(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.LimitRanges, err = client.LimitRanges(corev1.NamespaceAll).List(filter.NamespaceOptions(opts))
+		objects.LimitRanges, err = client.LimitRanges(corev1.NamespaceAll).List(gCtx, filter.NamespaceOptions(opts))
 		return
 	})
 	g.Go(func() (err error) {
-		objects.SystemNamespace, err = client.Namespaces().Get(metav1.NamespaceSystem, metav1.GetOptions{})
+		objects.SystemNamespace, err = client.Namespaces().Get(gCtx, metav1.NamespaceSystem, metav1.GetOptions{})
 		return
 	})
 	g.Go(func() (err error) {
-		objects.MutatingWebhookConfigurations, err = admissionControllerClient.MutatingWebhookConfigurations().List(opts)
+		objects.MutatingWebhookConfigurations, err = admissionControllerClient.MutatingWebhookConfigurations().List(gCtx, opts)
 		return
 	})
 	g.Go(func() (err error) {
-		objects.ValidatingWebhookConfigurations, err = admissionControllerClient.ValidatingWebhookConfigurations().List(opts)
+		objects.ValidatingWebhookConfigurations, err = admissionControllerClient.ValidatingWebhookConfigurations().List(gCtx, opts)
 		return
 	})
 	g.Go(func() (err error) {
-		objects.Namespaces, err = client.Namespaces().List(opts)
+		objects.Namespaces, err = client.Namespaces().List(gCtx, opts)
 		return
 	})
 
